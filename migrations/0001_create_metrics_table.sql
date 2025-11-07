7f459b826e3d6f730da7855d7552e694a1e41466
Felipe A. <73603452+imfelipedev@users.noreply.github.com>
2025-11-07 20:10:29 -0300

    :recycle: refactor: project base

---
+++ migrations/0001_create_metrics_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS metrics (
+  ip_hash TEXT NOT NULL,
+  video_id TEXT NOT NULL,
+  last_watch_time INTEGER NOT NULL DEFAULT 0,
+  updated_at INTEGER NOT NULL DEFAULT (strftime('%s','now')),
+  PRIMARY KEY (ip_hash, video_id)
+);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import { renderHtml } from "./renderHtml";
+type BodyDTO = {
+  video_id: string;
+  time: number;
+}
 
 export default {
   async fetch(request, env) {
-    const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-    const { results } = await stmt.all();
+    if (request.method !== "POST") {
+      return new Response("method not allowed", { 
+        status: 405 
+      });
+    }
 
-    return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-      headers: {
-        "content-type": "text/html",
-      },
+    const body = await request.json() as BodyDTO;
+    if (!body.video_id || !body.time || typeof body.time !== "number") {
+      return new Response("invalid payload", { 
+        status: 400 
+      });
+    }
+
+    const clientIP = request.headers.get("cf-connecting-ip") || request.headers.get("x-forwarded-for") || "unknown";
+    const ipHash = await hash(clientIP + body.video_id);
+    await env.DB.prepare(
+      `
+      INSERT INTO metrics (ip_hash, video_id, last_watch_time)
+      VALUES (?1, ?2, ?3)
+      ON CONFLICT(ip_hash, video_id)
+      DO UPDATE SET
+        last_watch_time = MAX(last_watch_time, excluded.last_watch_time),
+        updated_at = strftime('%s','now')
+      `
+    ).bind(ipHash, body.video_id, body.time).run();
+
+    return new Response("ok", {
+      status: 201
     });
   },
 } satisfies ExportedHandler<Env>;
+
+async function hash(str: string) {
+  const encode = new TextEncoder().encode(str);
+  const buf = await crypto.subtle.digest("SHA-256", encode);
+  return btoa(String.fromCharCode(...new Uint8Array(buf)));
+}
